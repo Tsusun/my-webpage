b686265e95ded79d1ddfbcc17aee9339bec5a722
TsuSun <jumbobrave@protonmail.com>
2021-12-05 17:27:13 +0100

    Add darkmode

---
+++ src/components/hooks/ui/useDarkMode.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react"
+
+
+// Declarations below is the way I found to remove the ReferenceError: item/window is not defined
+let foo = ""
+let item = ""
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      if (typeof window !== "undefined") {
+        item = window.localStorage.getItem(key)
+      }
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
+
+  const setValue = (value) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value
+
+      setStoredValue(valueToStore)
+      if (typeof window !== "undefined") {
+        foo = window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return [storedValue, setValue]
+}
+
+const useDarkMode = () => {
+  const [enabled, setEnabled] = useLocalStorage("dark-theme")
+  const isEnabled = typeof enabledState === "undefined" && enabled
+
+  useEffect(() => {
+    const className = "dark"
+    if (typeof window !== "undefined") {
+      const bodyClass = window.document.body.classList
+    }
+    isEnabled ? bodyClass.add(className) : bodyClass.remove(className)
+  }, [enabled, isEnabled])
+
+  return [enabled, setEnabled]
+}
+
+export default useDarkMode
--- src/components/ui/common/navbar/index.js
+++ src/components/ui/common/navbar/index.js
@@ -1,21 +1,31 @@
-import { AiFillHome } from "react-icons/ai"
 import { Button } from "@components/ui/common"
+import useDarkMode from "@components/hooks/ui/useDarkMode"
+import { BsTsunami } from "react-icons/bs"
+import { AiFillHome } from "react-icons/ai"
+import { GiNinjaHead } from "react-icons/gi"
 
 export default function Navbar() {
+  const [darkTheme, setDarkTheme] = useDarkMode()
+  const handleMode = () => setDarkTheme(!darkTheme)
   return (
     <section>
       <div className="relative py-6 pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative self-auto" aria-label="Global">
           <div className="flex  justify-between items-center">
             <div>
-              <AiFillHome onClick={() => (document.location.href = "/")} size="24" className="navbar-icon" />
+              <AiFillHome onClick={() => (document.location.href = "/")} size="24" className="navbar-icon bg-black" />
             </div>
             <div className="flex">
-              <span className="px-2">
-                <Button onClick={() => window.open("https://metamask.io/download", "_blank")}>
-                  Install Metamask
-                </Button>
+              <span className="px-2" onClick={handleMode}>
+                {darkTheme ? (
+                  <BsTsunami size="24" className="navbar-icon " />
+                ) : (
+                  <GiNinjaHead size="24" className="navbar-icon bg-black" />
+                )}
               </span>
+              <Button onClick={() => window.open("https://metamask.io/download", "_blank")}>
+                Install Metamask
+              </Button>
             </div>
           </div>
         </nav>
