984e900e5bf3717aed556e6948254b6019c88f1d
TsuSun <jumbobrave@protonmail.com>
2021-12-06 19:32:50 +0100

    Add homepage title and icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import Hero from "@components/ui/hero"
 import { BaseLayout } from "@components/ui/layout"
 import Presentation from "@components/ui/presentation"
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
-      <Hero/>
-      <Presentation/>
-    </div>
 
+      <div>
+        <Head>
+        <title>Silvan Reigue - Homepage</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="/SR.png" />
+ 
+      </Head>
+        <Hero />
+        <Presentation />
+      </div>
   )
 }
 
